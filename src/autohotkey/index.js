86d344be8883f07e3feeb881e86111bcdc4ab167
Shadownrun <gustavooavila@gmail.com>
2021-10-25 00:09:17 -0300

    better error messages from autohotkey

--- src/autohotkey/index.js
+++ src/autohotkey/index.js
@@ -4,14 +4,26 @@ const { spawn } = require("child_process");
 const messaging = require("./messaging");
 const {plugins_path, interpreter_path} = require("../utils/paths");
 
-const stdio = ['pipe', 'pipe', process.stdout]
-const spawn_options = {/*windowsHide: true,*/ cwd: plugins_path, stdio };
+const spawn_options = {/*windowsHide: true,*/ cwd: plugins_path};
+
+class AutohotkeyError extends Error {
+    constructor(message, stack){
+        super(message);
+        this.message = message;
+        this.name = "AutohotkeyError";
+        this.stack = `${this.name}: ${this.message}\n${stack}`
+    }
+}
 
 class autohotkey{
     constructor(script, ...args){
-        args = [...args, script]
+        args = [...args, "/ErrorStdOut", script]
         this.process = spawn(interpreter_path, args, spawn_options)
         this.messaging = new messaging(this.process.stdin, this.process.stdout);
+        this.process.stderr.on("data", (data)=>{
+            data = data.toString().split(": ==> ");
+            throw new AutohotkeyError(data[1] || "", data[0]);
+        })
     }
     
     on(...params){return this.messaging.on(...params)}
