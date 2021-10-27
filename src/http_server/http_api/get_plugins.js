b1717cb732bb065f6049d55704451a240bbae701
Shadownrun <gustavooavila@gmail.com>
2021-10-27 16:55:25 -0300

    add plugin_path to get plugins api

--- src/http_server/http_api/get_plugins.js
+++ src/http_server/http_api/get_plugins.js
@@ -11,6 +11,7 @@ function api_get_plugins(plugin_list) {
         const response = JSON.stringify(
             plugins.map((plugin)=>({
                 icon_path : (()=>{icon_path = plugin.icon_path(); return icon_path ? path.relative(plugins_path, icon_path) : undefined})(),
+                plugin_path: path.relative(plugins_path, plugin.dir_path),
                 name: plugin.name
             }))
         );
